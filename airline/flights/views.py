4e1f54fbea05a9263ce00f5707371de9a53a78a4
Giga <23157357+Giagar@users.noreply.github.com>
2020-10-10 18:34:44 +0200

    done index page displaying data in Flight database; everything is working

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Ok")
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
